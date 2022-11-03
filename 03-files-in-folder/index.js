4fa426dd1962dbea5b19417d1279f8839f67aa8d
Denis Ermakov <alexmezhuev@yandex.by>
2022-11-03 23:03:17 +0300

    feat: 1,2,3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const {readdir, stat} = require('fs/promises')
+const path = require('path')
+
+async function filesFolder () {
+  try {
+    const folder = path.join(__dirname, 'secret-folder')
+    const files = await readdir(folder, {withFileTypes: true})
+    for (let file of files){
+      if (file.isFile()){
+        const fileStat = await stat(path.join(folder, file.name))
+        const fileName = file.name.split('.')[0]
+        const extFile = file.name.split('.')[1]
+        const sizeFile = fileStat.size / 1024
+        console.log(`${fileName} - ${extFile} - ${sizeFile}kb`);
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+filesFolder()
